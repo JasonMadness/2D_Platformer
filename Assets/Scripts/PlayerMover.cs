fc3b66adff3adc3b32e691bd22423e7c490b1b50
JasonMadness <tobc_madness@mail.ru>
2025-06-03 14:43:33 +0300

    add isGronded check

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,11 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _speed = 4f;
     [SerializeField] private float _jumpForce = 8f;
-    
+
+    private float _minGroundDistance = 0.2f;
+    private bool _isGrounded = false;
+
+    private void Update()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _minGroundDistance, _groundLayer);
+    }
+
     public void MoveRight()
     {
         transform.Translate(Vector2.right * _speed * Time.deltaTime);
@@ -18,6 +30,7 @@ public class PlayerMover : MonoBehaviour
 
     public void Jump()
     {
-        _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        if (_isGrounded)
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
